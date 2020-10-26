cc0768668843b72088b396d449ea20770d7127d1
ernestomedina17 <ernestomedina17@gmail.com>
2020-10-26 15:07:11 -0600

    adding terraform and ansible playbooks

---
+++ jenkins-dev-variables.tf
@@ -0,0 +1,9 @@
+variable "private_key_path" {
+  description = "Path to the private SSH key, used to access the instance."
+  default     = "~/.ssh/google_compute_engine"
+}
+
+variable "ssh_user" {
+  description = "SVC which has roles/compute.osAdminLogin to ssh your instance."
+  default     = "sysadmin"
+}
---
+++ jenkins-dev.tf
@@ -0,0 +1,72 @@
+provider "google" {
+  # credentials = GOOGLE_APPLICATION_CREDENTIALS
+  # project     = CLOUDSDK_CORE_PROJECT
+  # region      = CLOUDSDK_COMPUTE_REGIONT
+  # zone	= CLOUDSDK_COMPUTE_ZONE
+}
+
+resource "google_compute_instance" "nginx_httpd" {
+  name         = "nginx_httpd"
+  machine_type = "g1-small"
+  tags = ["web"]
+
+  boot_disk {
+    initialize_params {
+      image = "rhel-cloud/rhel7-ansible"
+    }
+  }
+
+  // Local SSD disk
+  # scratch_disk {}
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  metadata = {
+    Name     = "OS Login for svc accounts"
+    enable-oslogin = "TRUE"
+  }
+
+  # metadata_startup_script = "echo hi > /test.txt"
+
+  # service_account {
+  #   scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  # }
+
+  provisioner "remote-exec" {
+    inline = ["echo 'running ansible playbook'"]
+
+    connection {
+      type        = "ssh"
+      user        = var.ssh_user
+      private_key = file(var.private_key_path)
+      host        = google_compute_instance.nginx_httpd.network_interface.0.access_config.0.nat_ip
+    }
+  }
+  provisioner "local-exec" {
+    command = "ansible-playbook -i '${google_compute_instance.nginx_httpd.network_interface.0.access_config.0.nat_ip},' --private-key ${var.private_key_path} nginx_httpd.yml"
+  }
+}
+
+resource "google_compute_firewall" "default" {
+  name    = "web-firewall"
+  network = "default"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["web"]
+}
+
